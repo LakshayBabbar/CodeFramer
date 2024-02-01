6e53f0ca9d02bed7a5ee5656f27f491ce6ee34f3
LakshayBabbar <lakshaybabbar0118@gmail.com>
2024-02-01 22:31:07 +0530

    Removing useless content

--- src/app/dashboard/[slug]/page.js
+++ src/app/dashboard/[slug]/page.js
@@ -6,11 +6,7 @@ import { useContext, useEffect, useState } from "react";
 
 const page = ({ params }) => {
   const { data } = useContext(UserContext);
-  const [editorData, setEditorData] = useState({
-    html: "<h1>Welcome to CodeFramer</h1>",
-    css: "h1{color: red}",
-    js: "document.getElementsByTagName('h1').color = 'green'",
-  });
+  const [editorData, setEditorData] = useState({});
   useEffect(()=>{
     const selectedData = data.find((item)=>{
       return item.id === params.slug
