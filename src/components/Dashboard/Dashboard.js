351845ded972f76649178392fb4c1515202ec1ab
LakshayBabbar <lakshaybabbar0118@gmail.com>
2024-02-01 18:24:39 +0530

    Change the Dashboard UI

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,11 +4,17 @@ import { useState, useEffect, useContext } from "react";
 import { db } from "../../../lib/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { UserContext } from "@/context";
-import Card from "../UI/Card";
+import Link from "next/link";
+import Button from "../UI/Button";
+import { FaCode } from "react-icons/fa";
+import { IoMdAdd } from "react-icons/io";
+import { MdOutlineDelete } from "react-icons/md";
+import Image from "next/image";
+import avatar from "../../../public/avatar.svg";
 
 const Dashboard = ({ username }) => {
   const [userData, setUserData] = useState([]);
-  const { setData, data } = useContext(UserContext);
+  const { setData } = useContext(UserContext);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,22 +34,57 @@ const Dashboard = ({ username }) => {
 
       const userDataArray = await Promise.all(userDataPromises);
       const flattenedUserData = userDataArray.flat();
-      setData(flattenedUserData[1]);
+      setData(flattenedUserData[0]);
       setUserData(flattenedUserData);
     };
     fetchData();
   }, []);
 
   return (
-    <div className={styles.wrapper}>
-    {console.log(userData)}
-      <h1>Projects</h1>
-      <ul>
-      {userData.map((elements) => {
-        return <li>{elements.name}</li>;
-      })}
-      </ul>
-    </div>
+    <>
+      <section className={styles.user}>
+        <div className={styles.usersDetails}>
+          <Image src={avatar} alt="avatar pic" width="80" />
+          <div className={styles.userRight}>
+            <h1>Hello,</h1>
+            <h2>@{username}</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.project}>
+        <h1>Projects</h1>
+        <div className={styles.addProject}>
+          <IoMdAdd />
+          Add Project
+        </div>
+        <div className={styles.container}>
+          {userData.map((elements) => {
+            return (
+              <div className={styles.allProjects} key={elements.id}>
+                <h2>
+                  <FaCode />
+                  {elements.name}
+                </h2>
+                <p>{elements.desc}</p>
+                <div className={styles.projectButtons}>
+                  <Link href={`/dashboard/${elements.id}`}>
+                    <Button
+                      style={{ background: "var(--text)", color: "var(--bg)" }}
+                    >
+                      Go to Editor
+                    </Button>
+                  </Link>
+                  <MdOutlineDelete
+                    style={{ color: "red", fontSize: "1.5rem" }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 };
 export default Dashboard;
